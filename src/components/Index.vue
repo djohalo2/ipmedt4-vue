8f3f154139238610feb3b533e446a26aa7709dd4
Djovanni Tehubijuluw <djohalo2@hotmail.com>
2017-05-25 19:38:07 +0200

    Initial commit

---
+++ src/components/Index.vue
@@ -0,0 +1,31 @@
+<template>
+    <q-layout>
+        <div slot="header" class="toolbar">
+            <q-toolbar-title>
+                IPMEDT4
+            </q-toolbar-title>
+        </div>
+
+        <q-tabs slot="navigation" :refs="$refs" default-tab="tab-4">
+          <q-tab name="tab-1" icon="message">
+            Overzicht
+          </q-tab>
+          <q-tab name="tab-2" icon="fingerprint">
+            Informatie
+          </q-tab>
+          <q-tab name="tab-3" icon="alarm">
+            Tab 3
+          </q-tab>
+        </q-tabs>
+    </q-layout>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="styl">
+
+</style>
