5e5eecd316fa986a0993e64fabb79026c2a87e4a
Bas van Bovene <bas@bovene.nl>
2017-06-12 20:01:07 +0200

    route change

--- routes/api.php
+++ routes/api.php
@@ -39,7 +39,7 @@ $api->version('v1', ['middleware' => 'api.throttle', 'limit' => 10000, 'expires'
         $api->get('authenticate/checkuser', ['as' => 'authenticate.checkuser',  'uses' => '\App\Http\Controllers\AuthenticateController@authenticateCheck']);
 
         $api->get('patient/user/{user_id}', ['as' => 'patient.user_id',  'uses' => '\App\Http\Controllers\PatientController@patient_user_id']);
-        $api->get('doctor/user/{user_id}', ['as' => 'patient.user_id',  'uses' => '\App\Http\Controllers\DoctorController@doctor_user_id']);
+        $api->get('doctor/user/{user_id}', ['as' => 'doctor.user_id',  'uses' => '\App\Http\Controllers\DoctorController@doctor_user_id']);
     });
 
 
