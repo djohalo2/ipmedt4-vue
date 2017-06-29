1bbac2c3ae830bb79eee498014dd311b4ed4df82
Djovanni Tehubijuluw <djohalo2@hotmail.com>
2017-06-29 13:25:51 +0200

    Doctor medicine page is now connected to the database, medicines are fetched when component is created.

--- front-end/src/components/arts/Medicatie.vue
+++ front-end/src/components/arts/Medicatie.vue
@@ -7,7 +7,7 @@
             <div class="list bg-white striped">
               <div class="item"  v-for="(medicijn, index) in searchedMedicijnen" :key="index">
                 <div class="item-content">
-                  {{medicijn.naam}}
+                  {{medicijn.name}}
                 </div>
               </div>
             </div>
@@ -22,33 +22,29 @@ export default {
     name: 'medicatie',
     data() {
       return {
-        medicijnSearch: "",
-        medicijnen: [
-          {
-            naam: "ibuprofen"
-          }, {
-            naam: "paracetamol"
-          }, {
-            naam: "amfetamine"
-          }
-        ]
+        medicijnSearch: ''
       }
     },
     computed: {
-        searchedMedicijnen() {
-            if(this.medicijnSearch !== ""){
-                let medicijnen = [];
-                this.medicijnen.map(medicijn => {
-                    // Djo, hij zegt dat er iets undefined is, maar snap niet zo goed wat.
-                    if(_.includes(medicijn.naam.toLowerCase(), this.medicijnSearch.toLowerCase())){
-                        medicijnen.push(medicijn);
-                    }
-                });
-                return medicijnen;
-            } else {
-                return this.medicijnen;
-            }
-        }
+      medicatie () {
+        return this.$store.getters.getAllMedicines
+      },
+      searchedMedicijnen() {
+          if(this.medicijnSearch !== ""){
+              let medicatie = [];
+              this.medicatie.map(medicijn => {
+                  if(_.includes(medicijn.name.toLowerCase(), this.medicijnSearch.toLowerCase())){
+                      medicatie.push(medicijn);
+                  }
+              });
+              return medicatie;
+          } else {
+              return this.medicatie;
+          }
+      }
+    },
+    created () {
+      this.$store.dispatch('FETCH_ALL_MEDICINES')
     }
 }
 
