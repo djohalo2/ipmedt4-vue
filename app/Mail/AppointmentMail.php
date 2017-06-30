ca4ac85d7a930fd98d01be92f49affdbe4891185
Bas van Bovene <bas@bovene.nl>
2017-06-30 12:27:28 +0200

    ics file with appointment email

--- app/Mail/AppointmentMail.php
+++ app/Mail/AppointmentMail.php
@@ -30,9 +30,65 @@ class AppointmentMail extends Mailable implements ShouldQueue
      */
     public function build()
     {
+
+        $ical = '';
+
+        $ical .= 'BEGIN:VCALENDAR' . "\xA";
+        $ical .= 'VERSION:2.0' . "\xA";
+        $ical .= 'PRODID:-//Mijn nazorg//EN"' . "\xA";
+        $ical .= 'CALSCALE:GREGORIAN' . "\xA";
+        $ical .= 'METHOD:PUBLISH' . "\xA";
+        $ical .= 'BEGIN:VTIMEZONE' . "\xA";
+        $ical .= 'TZID:Europe/Amsterdam' . "\xA";
+        $ical .= 'BEGIN:STANDARD' . "\xA";
+        $ical .= 'DTSTART:20151025T020000' . "\xA";
+        $ical .= 'RDATE:20160327T030000' . "\xA";
+        $ical .= 'TZOFFSETFROM:+0200' . "\xA";
+        $ical .= 'TZOFFSETTO:+0100' . "\xA";
+        $ical .= 'TZNAME:Europe/Amsterdam CET' . "\xA";
+        $ical .= 'END:STANDARD' . "\xA";
+        $ical .= 'BEGIN:STANDARD' . "\xA";
+        $ical .= 'DTSTART:20161030T020000' . "\xA";
+        $ical .= 'RDATE:20170326T030000' . "\xA";
+        $ical .= 'TZOFFSETFROM:+0200' . "\xA";
+        $ical .= 'TZOFFSETTO:+0100' . "\xA";
+        $ical .= 'TZNAME:Europe/Amsterdam CET' . "\xA";
+        $ical .= 'END:STANDARD' . "\xA";
+        $ical .= 'BEGIN:DAYLIGHT' . "\xA";
+        $ical .= 'DTSTART:20150601T073000' . "\xA";
+        $ical .= 'RDATE:20151025T020000' . "\xA";
+        $ical .= 'TZOFFSETFROM:+0100' . "\xA";
+        $ical .= 'TZOFFSETTO:+0200' . "\xA";
+        $ical .= 'TZNAME:Europe/Amsterdam CEST' . "\xA";
+        $ical .= 'END:DAYLIGHT' . "\xA";
+        $ical .= 'BEGIN:DAYLIGHT' . "\xA";
+        $ical .= 'DTSTART:20160327T030000' . "\xA";
+        $ical .= 'RDATE:20161030T020000' . "\xA";
+        $ical .= 'TZOFFSETFROM:+0100' . "\xA";
+        $ical .= 'TZOFFSETTO:+0200' . "\xA";
+        $ical .= 'TZNAME:Europe/Amsterdam CEST' . "\xA";
+        $ical .= 'END:DAYLIGHT' . "\xA";
+        $ical .= 'END:VTIMEZONE' . "\xA";
+        $ical .= 'BEGIN:VEVENT' . "\xA";
+        $ical .= 'TZID:Europe/Paris' . "\xA";
+
+        $ical .= 'UID:' . $this->data['id'] . "\xA";
+        $ical .= 'DTSTAMP;TZID=Europe/Amsterdam:' . $this->data['start_date_time'] . "\xA";
+        $ical .= 'ORGANIZER;CN=Mijn nazorg:MAILTO:no-reply@mijnnazorg.nl' . "\xA";
+        $ical .= 'LOCATION:LUMC Albinusdreef 2, 2333 ZA Leiden' . "\xA";
+        $ical .= 'DTSTART;TZID=Europe/Amsterdam:' . $this->data['start_date_time'] . "\xA";
+        $ical .= 'DTEND;TZID=Europe/Amsterdam:' . $this->data['end_date_time'] . "\xA";
+        $ical .= 'SUMMARY:Afspraak mijn nazorg' . "\xA";
+        $ical .= 'END:VEVENT' . "\xA";
+        $ical .= 'END:VCALENDAR' . "\xA";
+
+
         return $this->view('appointment')
             ->from('no-reply@mijnnazorg.nl')
             ->subject('Nieuwe afspraak | mijn nazorg')
+            ->attachData($ical, 'afspraak.ics', [
+                'mime' => 'text/calendar',
+            ])
             ->with([
                 'data' => $this->data
             ]);
