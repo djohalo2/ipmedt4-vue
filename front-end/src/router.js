8a7f18f4d0e9b30754dbd99e4b722c9c26e21b9c
Djovanni Tehubijuluw <djohalo2@hotmail.com>
2017-07-06 18:04:43 +0200

    Fix patient router links

--- front-end/src/router.js
+++ front-end/src/router.js
@@ -21,10 +21,8 @@ export default new VueRouter({
     { path: '/login', component: load('Login') },
     { path: '/patientinformatie', component: load('patient/PatientInformatie') },
     { path: '/behandeling/:typeBehandeling', component: load('BehandelingPage') },
-    { path: '/behandeling/:typeBehandeling/notities', component: load('NotitiesPage') },
-    { path: '/behandeling/:typeBehandeling/medicatie', component: load('Medicatie') },
-    { path: '/behandeling/:typeBehandeling/medicatie/:typeMedicijn', component: load('MedicatiePage') },
-    { path: '/behandeling/:typeBehandeling/oefeningen/:typeOefening', component: load('OefeningPage') },
+    { path: '/behandeling/:typeBehandeling/medicatie/:typeMedicijn', component: load('patient/medicatie/MedicatiePage') },
+    { path: '/behandeling/:typeBehandeling/oefeningen/:typeOefening', component: load('patient/oefeningen/OefeningPage') },
     // Paths voor arts
     { path: '/arts', component: load('arts/dashboard/Dashboard') },
     { path: '/arts/patienten', component: load('arts/patient/Patienten') },
